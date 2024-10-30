91010a375584676139b4dff68e0a302a2b4184da
digitalrebel85 <48913714+digitalrebel85@users.noreply.github.com>
2024-10-30 10:58:11 +0000

    Update page.tsx

--- app/advisor-types/page.tsx
+++ app/advisor-types/page.tsx
@@ -20,7 +20,7 @@ export default function AdvisorTypesPage() {
             <p className="mx-auto max-w-[700px] text-zinc-200 md:text-xl">
               Find the right professional for your financial needs
             </p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/Diq3QdbWpJI?si=StOUcr-BMtU3R_Jv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+           
           </div>
         </div>
       </section>
